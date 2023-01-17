318940943072162c8af31e76c7ed6ade5ac2a169
payalpagaria <pagariapayal67@gmail.com>
2023-01-17 17:34:26 +0530

    update file

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import bodyParser from "body-parser";
+import userRoutes from "./routes/users.js";
 
 const app=express();
 const PORT=3000;
 app.use(bodyParser.json())
+app.use("/users",userRoutes);
 app.get("/",(req,res)=>res.send("Welcome to the users Api"))
 app.listen(PORT,()=>console.log(`The server is started on port:http://localhost:${PORT}`));
