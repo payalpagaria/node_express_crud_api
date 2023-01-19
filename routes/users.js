00af7fad17da278949f97ed8dd5bd36e5cfe0ca3
payalpagaria <pagariapayal67@gmail.com>
2023-01-20 00:09:21 +0530

    Added delete route

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 
 const router=express.Router()
-const users=[]
+let users=[]
 router.get('/',(req,res)=>{
     console.log(users);
     res.send(users);
@@ -19,5 +19,9 @@ router.get('/:id',(req,res)=>{
     const {id}=req.params;
     const founduser=users.find((user)=>user.id===id);
     res.send(founduser);
+})
+router.delete('/:id',(req,res)=>{
+    console.log(`user with id ${req.params.id} has been deleted`);
+    users = users.filter((user) => user.id != req.params.id);
 })
 export default router;
