2c1b8a011db1ac2f40ae1633abc35da13bd68e26
payalpagaria <pagariapayal67@gmail.com>
2023-01-19 23:15:41 +0530

    Post route added

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,18 @@
 import  express  from "express";
 const router=express.Router()
-
+const users=[{
+    Fname:"payal",
+    Lname:"pagariya",
+    age:22
+}]
 router.get('/',(req,res)=>{
-    res.send('Hello');
+    console.log(users);
+    res.send(users);
 })
+router.post('/',(req,res)=>{
+    res.send("hello from post");
+    const user=req.body;
+    users.push(user);
+});
 
 export default router;
