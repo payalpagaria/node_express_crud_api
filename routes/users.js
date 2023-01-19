4c9a57ec3f1ac1836628653eb6ffbfccbd7e3951
payalpagaria <pagariapayal67@gmail.com>
2023-01-20 00:34:33 +0530

    upadte user has been added

--- routes/users.js
+++ routes/users.js
@@ -1,27 +1,15 @@
 import  express  from "express";
-import { v4 as uuidv4 } from 'uuid';
-
+import { getUsers,deleteUser,updateUser,getUser ,createUser } from "../controllers/users.js";
 
 const router=express.Router()
-let users=[]
-router.get('/',(req,res)=>{
-    console.log(users);
-    res.send(users);
-})
-router.post('/',(req,res)=>{
-    const user=req.body;
-    users.push({...user,id:uuidv4()});
-    res.send(users);
+router.get('/', getUsers);
 
-    
-});
-router.get('/:id',(req,res)=>{
-    const {id}=req.params;
-    const founduser=users.find((user)=>user.id===id);
-    res.send(founduser);
-})
-router.delete('/:id',(req,res)=>{
-    console.log(`user with id ${req.params.id} has been deleted`);
-    users = users.filter((user) => user.id != req.params.id);
-})
+router.post('/', createUser);
+
+router.get('/:id', getUser);
+
+router.delete('/:id', deleteUser);
+
+router.patch('/:id', updateUser);
+
 export default router;
